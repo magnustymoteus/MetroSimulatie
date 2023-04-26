1b6a72be74afec084cb4568fa2c82ebd261a5c80
gruzi <gruzixpotato@gmail.com>
2023-04-26 21:08:09 +0200

    more to do's

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 #include "Metronet/MetronetImporter.h"
 #include "components/Metronet/MetronetExporter.h"
 int main() {
+    // TODO: hollywood principle for everything
+    // TODO: validator for Station and TramType
+    // TODO: more consistency checks
+    // TODO: more preconditions and postconditions for methods
+    // TODO: write contract description for each class and method
+    // TODO: (maybe) a separate class for messages
     MetronetImporter parser;
     Metronet* metronet = parser.parseFile("input/input.xml");
     MetronetExporter::outputMetronet(metronet, "output/output.txt");
