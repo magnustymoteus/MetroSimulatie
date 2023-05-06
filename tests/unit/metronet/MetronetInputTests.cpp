0c29e08056e9b07986713de1c34f6bc4242ac0bb
darik0d <teleginaaleksandra1@gmail.com>
2023-05-06 21:10:08 +0200

    + 5 tests

--- tests/unit/metronet/MetronetInputTests.cpp
+++ tests/unit/metronet/MetronetInputTests.cpp
@@ -72,3 +72,41 @@ TEST_F(MetronetInputTest, DifferentTagsOrder) {
     EXPECT_NO_THROW(importer_.parseFile(filePath+"input/differentTagsOrderTest.xml"));
     EXPECT_TRUE(importer_.properlyInitialized());
 }
+
+TEST_F(MetronetInputTest, ReadTramsAndStationsTypes){
+    EXPECT_TRUE(importer_.properlyInitialized());
+    EXPECT_NO_THROW(importer_.parseFile(filePath+"input/readTramsAndStationsTypesTest.xml"));
+    EXPECT_TRUE(importer_.properlyInitialized());
+}
+TEST_F(MetronetInputTest, ReadTramsAndStationsTypes2){
+    EXPECT_TRUE(importer_.properlyInitialized());
+    EXPECT_NO_THROW(importer_.parseFile(filePath+"input/readTramsAndStationsTypesTest.xml"));
+    Metronet* metronet = importer_.parseFile(filePath+"input/readTramsAndStationsTypesTest.xml");
+    EXPECT_EQ(metronet->getStations().size(), 3);
+    EXPECT_EQ(metronet->getTrams().size(), 3);
+    EXPECT_EQ(metronet->getTramTypes().size(), 3);
+    EXPECT_TRUE(importer_.properlyInitialized());
+}
+TEST_F(MetronetInputTest, ReadTramsAndStationsTypes3){
+    EXPECT_TRUE(importer_.properlyInitialized());
+    EXPECT_NO_THROW(importer_.parseFile(filePath+"input/readTramsAndStationsTypesTest.xml"));
+    Metronet* metronet = importer_.parseFile(filePath+"input/readTramsAndStationsTypesTest.xml");
+    EXPECT_TRUE(metronet->getIsConsistent());
+    EXPECT_TRUE(importer_.properlyInitialized());
+}
+TEST_F(MetronetInputTest, ReadTramsAndStationsTypes4){
+    EXPECT_TRUE(importer_.properlyInitialized());
+    EXPECT_NO_THROW(importer_.parseFile(filePath+"input/readTramsAndStationsTypesTest.xml"));
+    Metronet* metronet = importer_.parseFile(filePath+"input/readTramsAndStationsTypesTest.xml");
+    EXPECT_NO_THROW(metronet->autoSimulate(3));
+    EXPECT_TRUE(importer_.properlyInitialized());
+}
+TEST_F(MetronetInputTest, ReadTramsAndStationsTypes5){
+    EXPECT_TRUE(importer_.properlyInitialized());
+    EXPECT_NO_THROW(importer_.parseFile(filePath+"input/readTramsAndStationsTypesTest.xml"));
+    Metronet* metronet = importer_.parseFile(filePath+"input/readTramsAndStationsTypesTest.xml");
+    EXPECT_TRUE(metronet->tramExists(12));
+    EXPECT_TRUE(metronet->tramExists(21));
+    EXPECT_FALSE(metronet->tramExists(666));
+    EXPECT_TRUE(importer_.properlyInitialized());
+}
